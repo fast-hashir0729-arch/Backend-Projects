269c18d0997329f64c03cae370d08d1387deea26
fast-hashir0729-arch <ahmedhashir083@gmail.com>
2026-07-19 19:53:44 +0500

    Stage 5: Swagger UI

--- CRUD-Operations/main.py
+++ CRUD-Operations/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException, status
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
-app = FastAPI()
+app = FastAPI(title = "Task API", description="A simple CRUD API for managing tasks using FastAPI.", version="1.0.0");
 
 
 tasks = [
@@ -24,7 +24,10 @@ tasks = [
 
 
 
-@app.get("/")
+@app.get("/",
+    summary="API information",
+    description="Returns basic information about the Task API."
+)
 def root():
     return {
         "name": "Task API",
@@ -36,7 +39,10 @@ def root():
 
 
 
-@app.get("/health")
+@app.get("/health",
+    summary="Health check",
+    description="Checks whether the API server is running."
+)
 def health():
     return {
         "status": "ok"
@@ -44,13 +50,19 @@ def health():
 
 
 
-@app.get("/tasks")
+@app.get("/tasks", 
+    summary="Get all tasks", 
+    description="Returns the complete list of tasks."
+)
 def get_tasks():
     return tasks
 
 
 
-@app.get("/tasks/{id}")
+@app.get("/tasks/{id}",
+    summary="Get a task by ID",
+    description="Returns a single task using its unique ID."
+)
 def get_task(id: int):
     for task in tasks:
         if task["id"] == id:
@@ -64,14 +76,31 @@ def get_task(id: int):
 
 
 class TaskCreate(BaseModel):
-    title: str
+    title: str = Field(
+        ...,
+        example="Buy milk",
+        description="The title of the task."
+    )
 
 
 class TaskUpdate(BaseModel):
-    title: str
-    done: bool
+    title: str = Field(
+        ...,
+        example="Buy groceries",
+        description="Updated title of the task."
+    )
 
-@app.post("/tasks", status_code=status.HTTP_201_CREATED)
+    done: bool = Field(
+        ...,
+        example=True,
+        description="Whether the task has been completed."
+    )
+
+@app.post("/tasks", 
+    status_code=status.HTTP_201_CREATED,     
+    summary="Create a new task",
+    description="Creates a new task with a unique ID and marks it as not completed"
+)
 def create_task(task: TaskCreate):
 
     if not task.title.strip():
@@ -93,7 +122,12 @@ def create_task(task: TaskCreate):
 
 
 
-@app.put("/tasks/{id}")
+@app.put("/tasks/{id}",     
+    summary="Update a task",
+    description="Updates the title and completion status of an existing task."
+)
+
+
 def update_task(id: int, updated_task: TaskUpdate):
 
     for task in tasks:
@@ -111,7 +145,11 @@ def update_task(id: int, updated_task: TaskUpdate):
     )
 
 
-@app.delete("/tasks/{id}", status_code=status.HTTP_204_NO_CONTENT)
+@app.delete("/tasks/{id}", 
+    status_code=status.HTTP_204_NO_CONTENT,    
+    summary="Delete a task",
+    description="Deletes a task using its ID."
+)
 def delete_task(id: int):
 
     for index, task in enumerate(tasks):
