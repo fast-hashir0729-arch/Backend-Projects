f2d76b3ffcdd124e41da150c8c5b14add5d4fd33
fast-hashir0729-arch <ahmedhashir083@gmail.com>
2026-07-19 13:59:32 +0500

    Stage 2: read endpoints with 404

--- CRUD-Operations/main.py
+++ CRUD-Operations/main.py
@@ -1,6 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
+
+
+tasks = [
+    {
+        "id": 1,
+        "title": "Study FastAPI",
+        "done": False
+    },
+    {
+        "id": 2,
+        "title": "Go to the gym",
+        "done": True
+    },
+    {
+        "id": 3,
+        "title": "Complete internship assignment",
+        "done": False
+    }
+]
 
 
 
@@ -21,3 +40,23 @@ def health():
     return {
         "status": "ok"
     }
+
+
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
+
+
+
+@app.get("/tasks/{id}")
+def get_task(id: int):
+    for task in tasks:
+        if task["id"] == id:
+            return task
+
+    raise HTTPException(
+        status_code=404,
+        detail=f"Task {id} not found"
+    )
+
