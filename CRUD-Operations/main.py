805239a2af8f509acf707a8c349f1abed7859742
fast-hashir0729-arch <ahmedhashir083@gmail.com>
2026-07-19 15:54:30 +0500

    Stage 3: create with validation

--- CRUD-Operations/main.py
+++ CRUD-Operations/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -60,3 +61,28 @@ def get_task(id: int):
         detail=f"Task {id} not found"
     )
 
+
+
+class TaskCreate(BaseModel):
+    title: str
+
+
+@app.post("/tasks", status_code=status.HTTP_201_CREATED)
+def create_task(task: TaskCreate):
+
+    if not task.title.strip():
+        raise HTTPException(
+            status_code=400,
+            detail="Title cannot be empty"
+        )
+    
+    
+    new_task = {
+        "id": len(tasks) + 1,
+        "title": task.title,
+        "done": False
+    }
+
+    tasks.append(new_task)
+
+    return new_task
